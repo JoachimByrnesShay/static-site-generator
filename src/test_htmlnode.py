d67ca1cd1031c2d04d966eeafb639fee0241081c
Joachim Byrnes-Shay <56098202+JoachimByrnesShay@users.noreply.github.com>
2025-03-03 14:18:30 -0500

    add more tests test_htmlnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -6,3 +6,14 @@ class TestHTMLNode(unittest.TestCase):
       node = HTMLNode("a", "great link", None, {"lang": "en", "src": "www.googleawho.com", "target": "_blank"})
       expected = ' lang="en" src="www.googleawho.com" target="_blank"'
       self.assertEqual(expected, node.props_to_html())
+
+   def test_repr(self):
+      children = [HTMLNode("a", "great link", None, {"src": "www.yahoo.com"})]
+      node = HTMLNode("p", "awesome paragraph", children, None)
+      expected = f'HTMLNode(p, awesome paragraph, {children}, None)'
+      self.assertEqual(expected, repr(node))
+
+   def test_child_repr(self):
+      node = HTMLNode("div", None, [HTMLNode("p", "really great", None, None)], None)
+      expected = f'HTMLNode(p, really great, None, None)'
+      self.assertEqual(repr(node.children[0]), expected)
