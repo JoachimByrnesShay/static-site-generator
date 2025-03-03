eebcc68cc48431ff3396d7a815f973235ae398da
Joachim Byrnes-Shay <56098202+JoachimByrnesShay@users.noreply.github.com>
2025-03-03 16:45:06 -0500

    add LeafNode with tests and ParentNode with tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -7,9 +7,11 @@ class HTMLNode:
         self.props = props 
     
     def to_html(self):
-        raise NotImplementedError 
+        raise NotImplementedError("to_html method not implemented")
     
     def props_to_html(self):
+        if not self.props:
+            return ""
         html = "" 
         for prop in self.props:
             html += f' {prop}="{self.props[prop]}"'
@@ -19,4 +21,37 @@ class HTMLNode:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
     
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("leaf node requires value")
+        elif self.tag is None:
+            return self.value 
+        else:
+            return (
+                f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+            )
+   
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("parent node requires tag")
+        elif self.children is None:
+            raise ValueError("parent node requires children")
+        else:
+            child_html = ""
+            for child in self.children:
+                child_html += child.to_html() 
+            return f'<{self.tag}{self.props_to_html()}>{child_html}</{self.tag}>'
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest 
-from htmlnode import HTMLNode 
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
    def test_props_to_html(self):
@@ -17,3 +17,69 @@ class TestHTMLNode(unittest.TestCase):
       node = HTMLNode("div", None, [HTMLNode("p", "really great", None, None)], None)
       expected = f'HTMLNode(p, really great, None, None)'
       self.assertEqual(repr(node.children[0]), expected)
+
+   def test_props_none_to_html(self):
+      node = HTMLNode("p", "great paragraph", None, None)
+      expected = ""
+      self.assertEqual(expected, node.props_to_html())
+
+class TestLeafNode(unittest.TestCase):
+   def test_leaf_to_html_p(self):
+      node = LeafNode("p", "Hello, world!")
+      self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+   def test_leaf_to_html_no_tag(self):
+      node = LeafNode(None, "hi world")
+      self.assertEqual(node.to_html(), "hi world")
+
+   def test_leaf_to_html_a(self):
+      node = LeafNode("a", "link to stuff", {"src":"https://www.greatlink.com"})
+      expected = '<a src="https://www.greatlink.com">link to stuff</a>'
+      self.assertEqual(node.to_html(), expected)
+
+   def test_leaf_to_html_no_value_with_exception(self):
+      node = LeafNode("div", None)
+      with self.assertRaises(Exception) as context:
+         node.to_html()
+      self.assertTrue("leaf node requires value" in str(context.exception))
+
+class TestParentNode(unittest.TestCase):
+   def test_parent_node_to_html(self):
+      children = [
+         LeafNode("b", "Bold text"),
+         LeafNode(None, "Normal text"),
+         LeafNode("i", "italic text"),
+         LeafNode(None, "Normal text")
+      ]
+      node = ParentNode("p", children)
+      expected = "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>"
+      self.assertEqual(expected, node.to_html())
+
+
+   def test_parent_node_to_html_nested_child(self):
+      child_with_children = ParentNode("div", [LeafNode("a", "a link", {"src": "https://www.linkadoo.net"})])
+      children = [
+         LeafNode("p", "great stuff"),
+         ParentNode("div", [LeafNode("p", "first p in a div"), LeafNode("p", "second p in a div"), child_with_children]),
+      ]
+      node = ParentNode("div", children)
+      expected = '<div><p>great stuff</p><div><p>first p in a div</p><p>second p in a div</p><div><a src="https://www.linkadoo.net">a link</a></div></div></div>'
+      self.assertEqual(expected, node.to_html())
+
+   def test_parent_node_repr(self):
+      self.maxDiff = None
+      child_with_children = ParentNode("div", [LeafNode("a", "a link", {"src": "https://www.linkadoo.net"})])
+      children = [
+         LeafNode("p", "great stuff"),
+         ParentNode("div", [LeafNode("p", "first p in a div"), LeafNode("p", "second p in a div"), child_with_children]),
+      ]
+      node = ParentNode("div", children)
+
+      expected_repr_inner_child_with_child = "ParentNode(div, [LeafNode(a, a link, {'src': 'https://www.linkadoo.net'})], None)"
+      expected_repr_outer_child_with_child = f"ParentNode(div, [LeafNode(p, first p in a div, None), LeafNode(p, second p in a div, None), {expected_repr_inner_child_with_child}], None)"
+      expected = f"ParentNode(div, [LeafNode(p, great stuff, None), {expected_repr_outer_child_with_child}], None)"
+   
+      self.assertEqual(repr(node), expected)
+
+if __name__ == "__main__":
+   unittest.main()
