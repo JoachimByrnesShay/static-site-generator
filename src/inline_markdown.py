8fc480ec59fcf41d9fe3f0cb24bd7d74df408c14
Joachim Byrnes-Shay <56098202+JoachimByrnesShay@users.noreply.github.com>
2025-03-03 21:17:33 -0500

    add tests for split_nodes_delimiter including for TextNode with multiple delimiters

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -3,6 +3,10 @@ from textnode import TextNode, TextType
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
+        # print("text type is: ", text_type)
+        if not node.text_type is TextType.TEXT:
+            new_nodes.append(node)
+            continue
         current_nodes = []
         splitted = node.text.split(delimiter)
         for ix, node_text in enumerate(splitted):
@@ -11,16 +15,15 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 current_nodes.append(TextNode(node_text, text_type))
         new_nodes.extend(current_nodes) 
-
     return new_nodes
 
 
 
-node = TextNode("This is text with a `code block` word", TextType.TEXT)
-new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-print(new_nodes)
-expected = [
-    TextNode("This is text with a ", TextType.TEXT),
-    TextNode("code block", TextType.CODE),
-    TextNode(" word", TextType.TEXT),
-]
+# node = TextNode("This is text with a `code block` word", TextType.TEXT)
+# new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+# print(new_nodes)
+# expected = [
+#     TextNode("This is text with a ", TextType.TEXT),
+#     TextNode("code block", TextType.CODE),
+#     TextNode(" word", TextType.TEXT),
+# ]
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -14,4 +14,44 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         
         self.assertEqual(new_nodes, expected)
 
+    def test_split_bold_block(self):
+        node = TextNode("this is text with a **bold section** for your enjoyment", TextType.TEXT)
+        expected = [
+            TextNode("this is text with a ", TextType.TEXT),
+            TextNode("bold section", TextType.BOLD),
+            TextNode(" for your enjoyment", TextType.TEXT),
+        ]
+
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_italic_block(self):
+        node = TextNode("this is _italic text for italic guys_ so that you may enjoy italics", TextType.TEXT)
+        expected = [
+            TextNode("this is ", TextType.TEXT),
+            TextNode("italic text for italic guys", TextType.ITALIC),
+            TextNode(" so that you may enjoy italics", TextType.TEXT),
+        ]
+
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_bold_and_italic_blocks(self):
+        node = TextNode("this has **bold text for me** and also _italic text for you_ with some other words", TextType.TEXT)
+        expected = [
+            TextNode("this has ", TextType.TEXT),
+            TextNode("bold text for me", TextType.BOLD),
+            TextNode(" and also ", TextType.TEXT),
+            TextNode("italic text for you", TextType.ITALIC),
+            TextNode(" with some other words", TextType.TEXT)
+        ]
+
+        with_bold_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        with_italic_and_bold_nodes = split_nodes_delimiter(with_bold_nodes, "_", TextType.ITALIC)
+     
+
+
+        self.assertEqual(with_italic_and_bold_nodes, expected)
 
