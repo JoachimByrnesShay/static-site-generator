aca18adffb97b191a232df9798e2ee9c38dba264
Joachim Byrnes-Shay <56098202+JoachimByrnesShay@users.noreply.github.com>
2025-04-09 23:16:35 -0400

    add continue statement to text_type condition; add first tests for inline_markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -5,14 +5,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
+            continue
 
         splitted = node.text.split(delimiter)
-        # "This is text with a `code block` word"
-        # "`code block` word"
-        # " `code block` word"
-        # "a `code block word"
-        # "`code block`"
-        # " `code block` word ` bad stuff"
         if len(splitted) % 2 == 0:
             raise ValueError("invalid markdown, delimiter is not closed")
         
@@ -27,13 +22,3 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(current_nodes)
     return new_nodes
 
-
-node = TextNode("This is text with a `code block` word", TextType.TEXT)
-new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-print(new_nodes)
-expected = [
-    TextNode("This is text with a ", TextType.TEXT),
-    TextNode("code block", TextType.CODE),
-    TextNode(" word", TextType.TEXT),
-]
-print(expected == new_nodes)
---
+++ src/test_inline_markdown.py
@@ -0,0 +1,30 @@
+import unittest 
+from textnode import TextNode, TextType
+from inline_markdown import split_nodes_delimiter
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_code(self):
+        node = TextNode("This is markdown text with a `code block`, ok?", TextType.TEXT)
+        actual = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+            TextNode("This is markdown text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(", ok?", TextType.TEXT),
+        ]
+
+        self.assertEqual(actual, expected)
+    
+    def test_bold_and_italic(self):
+        node = TextNode("This is markdown text with some **bold stuff here** and some _italic stuff here_, ok?", TextType.TEXT)
+        with_bold = split_nodes_delimiter([node], "**", TextType.BOLD)
+        actual_with_bold_and_italics = split_nodes_delimiter(with_bold, "_", TextType.ITALIC)
+        expected = [
+            TextNode("This is markdown text with some ", TextType.TEXT),
+            TextNode("bold stuff here", TextType.BOLD),
+            TextNode(" and some ", TextType.TEXT),
+            TextNode("italic stuff here", TextType.ITALIC),
+            TextNode(", ok?", TextType.TEXT),
+        ]
+
+        self.assertEqual(actual_with_bold_and_italics, expected)
+
