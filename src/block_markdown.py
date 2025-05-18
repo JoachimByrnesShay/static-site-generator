9ba1dca19be3f8628e20cabbe094971ff584f537
Joachim Byrnes-Shay <56098202+JoachimByrnesShay@users.noreply.github.com>
2025-05-18 17:37:54 -0500

    add more tests for block_to_block_type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -17,6 +17,7 @@ def block_to_block_type(markdown_block):
     lines = markdown_block.split("\n")
     if markdown_block.startswith("```") and markdown_block.endswith("```"):
         return BlockType.CODE 
+    
     if markdown_block.startswith(">"):
         quote = True 
         for line in lines:
@@ -25,6 +26,7 @@ def block_to_block_type(markdown_block):
                 break 
         if quote:
             return BlockType.QUOTE 
+    
     if markdown_block.startswith("- "):
         unordered_list = True 
         for line in lines:
@@ -33,6 +35,7 @@ def block_to_block_type(markdown_block):
                 break 
         if unordered_list:
             return BlockType.UNORDERED_LIST 
+    
     if markdown_block.startswith("#"):
         heading = True 
         for line in lines:
@@ -41,6 +44,7 @@ def block_to_block_type(markdown_block):
                 break 
         if heading:
             return BlockType.HEADING 
+    
     if markdown_block.startswith("1"):
         ordered_list = True 
         for num in range(1, len(lines) + 1):
@@ -50,15 +54,6 @@ def block_to_block_type(markdown_block):
                 break 
         if ordered_list:
             return BlockType.ORDERED_LIST 
+    
     return BlockType.PARAGRAPH
     
-
-
-
-# Create a block_to_block_type function that takes a single block of markdown text as input and returns the BlockType representing the type of block it is. You can assume all leading and trailing whitespace were already stripped (we did that in a previous lesson).
-# Headings start with 1-6 # characters, followed by a space and then the heading text.
-# Code blocks must start with 3 backticks and end with 3 backticks.
-# Every line in a quote block must start with a > character.
-# Every line in an unordered list block must start with a - character, followed by a space.
-# Every line in an ordered list block must start with a number followed by a . character and a space. The number must start at 1 and increment by 1 for each line.
-# If none of the above conditions are met, the block is a normal paragraph.
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -30,26 +30,50 @@ This is a new line in the same paragraph
         )
 
 class TestBlockToBlockType(unittest.TestCase):
-    def test_block_to_blocktype_heading(self):
+    def test_heading(self):
         markdown = "# this is a heading\n### this is another heading\n##### and this is a heading"
         block_type = block_to_block_type(markdown)
         expected = BlockType.HEADING 
         self.assertEqual(block_type, expected)
 
-    def test_block_to_blocktype_not_heading_if_incorrect_syntax(self):
+    def test_not_heading_if_too_many_hash(self):
         markdown = "# this is a heading\n### this is another heading\n####### and this looks like a heading but it has too many hash symbols at start of line"
         block_type = block_to_block_type(markdown) 
         expected = BlockType.PARAGRAPH 
         self.assertEqual(block_type, expected)
 
-    def test_block_to_blocktype_ordered_list(self):
+    def test_not_heading_if_line_starts_space(self):
+        markdown = "# this is a heading\n ### this is another heading\n### and this looks like a heading but it has too many hash symbols at start of line"
+        block_type = block_to_block_type(markdown) 
+        expected = BlockType.PARAGRAPH 
+        self.assertEqual(block_type, expected)
+
+    def test_ordered_list(self):
         markdown = "1. do this first thing\n2. do another thing\n3. do this third thing last"
         block_type = block_to_block_type(markdown)
         expected = BlockType.ORDERED_LIST 
         self.assertEqual(block_type, expected)
 
-    def test_block_to_blocktype_not_ordered_list_when_not_sequential(self):
+    def test_not_ordered_list_when_not_sequential(self):
         markdown = "1. do this first thing\n3. do another thing\n4. do this third thing last"
         block_type = block_to_block_type(markdown)
         expected = BlockType.PARAGRAPH
+        self.assertEqual(block_type, expected)
+
+    def test_unordered_list(self):
+        markdown = "- do this first thing\n- do another thing\n- do this third thing last"
+        block_type = block_to_block_type(markdown)
+        expected = BlockType.UNORDERED_LIST
+        self.assertEqual(block_type, expected)
+
+    def test_quote(self):
+        markdown = ">this is a great quote\n>and it has more than one line as well"
+        block_type = block_to_block_type(markdown)
+        expected = BlockType.QUOTE
+        self.assertEqual(block_type, expected)
+
+    def test_code(self):
+        markdown = "```def this_is_code(string):\n    new_string = string + ' its a new ending'\n    print(new_string)```"
+        block_type = block_to_block_type(markdown)
+        expected = BlockType.CODE
         self.assertEqual(block_type, expected)
